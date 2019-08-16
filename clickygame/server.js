32c895efa6d768184ca0b36d58d524e603ea8955
Joze Alcantara <krzduaz@yahoo.com>
2019-08-16 06:52:42 -0700

    Create server.js

---
+++ clickygame/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("my-app/build"));
+}
+
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
